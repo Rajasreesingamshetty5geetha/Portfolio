d73e421c9c084303997d817024936671d856c6a2
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2024-11-16 21:47:25 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/components/provider"
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Singamsetty Rajasree",
+  description: "Web Developer|UI/UX designer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/Grid.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BentoGrid } from './ui/BentoGrid'
+
+const Grid = () => {
+  return (
+    <div>
+      <section id="about">
+        
+      </section>
+    </div>
+  )
+}
+
+export default Grid
