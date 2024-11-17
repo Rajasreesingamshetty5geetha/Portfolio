e6de7138002aeb29c9bad00c664049b5e7cdf44c
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2024-11-18 01:56:54 +0530

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -100,11 +100,11 @@ export const BentoGridItem = ({
         <div className={cn(
           titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 '
         )}>
+           <div className="font-sans font-bold  text-lg lg:text-3xl max-w-96 z-10">
+            {title}
+          </div>
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10 dark:text-neutral-200 mb-2 mt-2" >
             {description}
-          </div>
-          <div className="font-sans font-bold  text-lg lg:text-3xl max-w-96 z-10">
-            {title}
           </div>
         
           {id === 3 && (
