3f34d53c0907f48f2f1247a4ff959659f598328c
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2026-03-14 23:50:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,32 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { ProjectsPage } from "@/components/Projects";
 import Techstack from "@/components/Techstack";
+import Experience from "@/components/Experience";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaCode, FaHome, FaProjectDiagram } from "react-icons/fa";
-import {MdAccountCircle, MdContactPage, MdDock, MdOutlineDock} from 'react-icons/md';
-import { Analytics } from "@vercel/analytics/react"
-
+import { MdAccountCircle, MdContactPage, MdOutlineDock, MdWork } from 'react-icons/md';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
-      <div className="max-w-7xl w-full ">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <FloatingNav
           navItems={[
-            {name:'Home',link:'/',icon:<FaHome/>
-            },{name:'About',link:'#about',icon:<MdAccountCircle/>},{name:'TechStack',link:'#tech',icon:<FaCode/>},{name:'Projects',link:'#Projects',icon:<MdOutlineDock/>},{name:'Contact',link:'#contact',icon:<MdContactPage/>}]}
+            { name: 'Home', link: '/', icon: <FaHome /> },
+            { name: 'About', link: '#about', icon: <MdAccountCircle /> },
+            { name: 'TechStack', link: '#tech', icon: <FaCode /> },
+            { name: 'Projects', link: '#Projects', icon: <MdOutlineDock /> },
+            { name: 'Experience', link: '#experience', icon: <MdWork /> },
+            { name: 'Contact', link: '#contact', icon: <MdContactPage /> },
+          ]}
         />
-       <Hero/>
-       <Grid/>
-       <Techstack/>
-       <ProjectsPage/>
-       <Footer/>
+        <Hero />
+        <Grid />
+        <Techstack />
+        <ProjectsPage />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
